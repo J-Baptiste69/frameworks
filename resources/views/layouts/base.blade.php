dfc593d032998c9f311d19689c279e2840638935
Your Name <jb.lauth@it-students.fr>
2021-03-15 18:29:13 +0100

    sauvegarde

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+</head>
+<body>
+    <header> ceci est un header </header>
+    <main>
+        @yield('content')
+    </main>
+    <footer>ceci est un footer</footer>
+</body>
+</html>
