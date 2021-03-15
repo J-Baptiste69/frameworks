880df4e40d9afbe38ff0dd80e7258339f808ba6d
J-Baptiste69 <75609930+J-Baptiste69@users.noreply.github.com>
2021-03-15 23:02:18 +0100

    Add files via upload

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -3,13 +3,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title')</title>
+    <link type="text/css" rel="stylesheet" href="HOMECSS.css">
+    <title>@yield('books')</title>
 </head>
 <body>
-    <header> ceci est un header </header>
+    <header> <ul id="nav">
+        <li> <a href="../">menu</a></li>
+        <li><a href="liste">liste des livres</a></li>
+        </ul> </header>
+        <img src="public/img/biblio">
     <main>
         @yield('content')
+        
     </main>
-    <footer>ceci est un footer</footer>
+    <footer>le footer est là!!!!</footer>
 </body>
 </html>
